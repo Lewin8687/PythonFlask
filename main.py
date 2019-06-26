0781e2fc3e3d3c4633bf5946165fc3e71cff610d
Lewin8687 <lewin8687@gmail.com>
2019-06-26 16:21:04 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,68 @@
-import keras
-from keras import backend as K
-from keras.models import Sequential
-from keras.models import load_model
+import tensorflow as tf 
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.models import model_from_json
 from flask import request
 from flask import jsonify
 from flask import Flask
+from numpy import array
+
+global graph, model
 
 app = Flask(__name__)
 
 def get_model():
-  global model
-  model = load_model('FirstTrainedModel.h5')
+  # Model reconstruction from JSON file
+  with open('model_architecture.json', 'r') as f:
+      model = model_from_json(f.read())
+
+  # Load weights into the new model
+  model.load_weights('model_weights.h5')
+
   print("Model loaded!")
 
-@app.route('/')
-def hello_world():
-  return 'Hey its Python Flask application!'
+#@app.route('/')
+#def hello_world():
+#  return 'Hey its Python Flask application!'
 
-@app.route('/predict', methods=['POST'])
+print("Loading model...")
+get_model()
+imdb = tf.keras.datasets.imdb
+# A dictionary mapping words to an integer index
+word_index = imdb.get_word_index()
+#graph = tf.get_default_graph()
+
+# The first indices are reserved
+word_index = {k:(v+3) for k,v in word_index.items()} 
+word_index["<PAD>"] = 0
+word_index["<START>"] = 1
+word_index["<UNK>"] = 2  # unknown
+word_index["<UNUSED>"] = 3
+
+@app.route('/predict', methods=["GET"])
 def predict():
-  content = request.get_json(force=True)
-  review = content['review']
-  
-  prediction = model.predict(review).tolist()
-  
-  response = {
-    'prediction':{
-      'result': prediction[0]
-    }
-  }
+  (train_data, train_labels), (test_data, test_labels) = imdb.load_data(num_words=10000)
+
+  print(train_data[0])
+
+  # Preprocess review
+  train_data = tf.keras.preprocessing.sequence.pad_sequences(train_data,
+                                                    value=word_index["<PAD>"],
+                                                    padding='post',
+                                                    maxlen=256)
+
+  print("******************1")
+
+  try:
+    print(model)
+    prediction = model.predict([train_data[0]])
+
+    print("**************************2")
+    print(prediction)
+
+    return prediction
+  except Exception as e:
+    print(e)
+    return e
 
 if __name__ == '__main__':
   app.run()
