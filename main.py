e169c47d582edacdc592970ef61cd14a1fba02cc
Dongyang Li <dongyang@heluxsystems.com>
2019-07-02 08:32:34 -0400

    Updated main.py

--- main.py
+++ main.py
@@ -20,6 +20,8 @@ word_index["<START>"] = 1
 word_index["<UNK>"] = 2  # unknown
 word_index["<UNUSED>"] = 3
 
+reverse_word_index = dict([(value, key) for (key, value) in word_index.items()])
+
 def get_model():
   # Model reconstruction from JSON file
   json_file = open('model_architecture.json','r')
@@ -32,17 +34,18 @@ def get_model():
   print("********Model loaded!")
   return model
 
+def decode_review(text):
+  return ' '.join([reverse_word_index.get(i, '?') for i in text])
+
 @app.route('/')
 def hello_world():
   return 'Hey its Python Flask application!'
 
-@app.route('/predict', methods=["GET"])
+@app.route('/dataset', methods=["GET"])
 def predict():
   index = int(request.args["index"])
 
   (train_data, train_labels), (test_data, test_labels) = imdb.load_data(num_words=10000)
-
-  print(train_data[index])
 
   # Preprocess review
   train_data = tf.keras.preprocessing.sequence.pad_sequences(train_data,
@@ -50,15 +53,43 @@ def predict():
                                                     padding='post',
                                                     maxlen=256)
 
+  print("***********" + decode_review(train_data[index]))
+
   try:
     # Get model
     model = get_model()
-    prediction = model.predict([train_data[index]]).tolist()
+    print(train_data[index])
+    prediction = model.predict_classes([[train_data[index]]])
 
-    return jsonify(prediction[0])
+    if prediction[0][0] == 0:
+      return "Bad review.."
+
+    return "Good review!"
   except Exception as e:
     print(e)
     return e
+
+@app.route('/predict', methods=["GET"])
+def predictReview():
+  review = str(request.args.get('content'))
+  print("*********" + review)
+  wordList = review.split(' ')
+
+  nums = [word_index.get(i, 0) for i in wordList]
+
+  data = tf.keras.preprocessing.sequence.pad_sequences([nums],
+                                                        value=word_index["<PAD>"],
+                                                        padding='post',
+                                                        maxlen=256)
+
+  # Get model
+  model = get_model()
+  prediction = model.predict_classes([data])
+
+  if prediction[0][0] == 0:
+    return "Bad review.."
+
+  return "Good review!"
 
 if __name__ == '__main__':
   app.run()
